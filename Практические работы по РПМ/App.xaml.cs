e5d560400cb378e1f8d01993e32654a6212b9bc5
KramSany <caha10-10@hotmail.com>
2022-12-26 23:08:13 +0300

    Добавьте файлы проекта.

---
+++ Практические работы по РПМ/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Практические_работы_по_РПМ
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
